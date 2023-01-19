30d34f6a6d95b80b4b5809c56422779629dc647b
Ananta Hembram <103342819+AnantaHembram@users.noreply.github.com>
2023-01-19 15:44:28 +0000

    Add files via upload

---
+++ First.java
@@ -0,0 +1,14 @@
+public class First
+public static void main(String[] args) {
+	
+}(String[]args) First{
+	{
+	int  i;
+	int  n;
+	i = 1 ;
+	n = 2 ;
+	int sum = i+n;
+	
+	System.out.println(sum=i+n);
+	    }
+}
